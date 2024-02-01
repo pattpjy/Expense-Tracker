1a3bebcfb77b5a1db8bde588f4c9aff91024d37a
Patt Sookmark <pijiya@sookmark.com>
2024-02-01 12:31:58 -0700

    Add modal

---
+++ src/components/GreetingModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="greeting">
+    <h1>Welcome to Budget Tracker Tool</h1>
+    <p>
+      This budget tracking tool designed to simplify your financial journey.
+      Take control of your money, effortlessly manage your income and expenses,
+      and stay on top of your financial goals.
+    </p>
+    <button @click="handleClosedModal">Close</button>
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+const emit = defineEmits(['closedModal']);
+
+const handleClosedModal = () => {
+  emit('closedModal');
+};
+</script>
